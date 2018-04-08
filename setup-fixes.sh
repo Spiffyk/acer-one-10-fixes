a6354ff4fd752a3d774bb9d62187c54a962a6b78
Oto Šťáva <oto.stava@gmail.com>
2018-04-08 14:55:19 +0200

    Fix screen brightness and refactor

--- setup-fixes.sh
+++ setup-fixes.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
+# Root user check
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo "This script must be run as root!" 
    exit 1
 fi
 
-echo "--- Copying intel firmware"
-cp intel/* /lib/firmware/i915/
+# Copy brcm firmware
+echo "--- Fixing Broadcom SDIO WiFi ---"
+cp -f firmware/brcm/* /lib/firmware/brcm/
 
-echo "--- Copying broadcom firmware"
-cp brcm/* /lib/firmware/brcm/
+# Blacklist HDMI audio
+echo "--- Fixing audio ---"
+echo "blacklist snd-hdmi-lpe-audio" > /etc/modprobe.d/blacklist-acer-hdmi.conf
 
-echo "--- Copying HDMI blacklist"
-cp sound/blacklist-acersnd.conf /etc/modprobe.d/
+# Defer i915
+echo "--- Fixing screen brightness ---"
+echo "blacklist i915" > /etc/modprobe.d/blacklist-acer-i915.conf
+echo "modprobe i915" > /etc/init.d/acer-i915-deferred
+chmod +x /etc/init.d/acer-i915-deferred
+ln -s ../init.d/acer-i915-deferred /etc/rc2.d/S99acer-i915-deferred
 
-echo "--- Updating initramfs"
-update-initramfs -u
+# Copy intel firmware
+echo "--- Fixing i915 firmware errors ---"
+cp -f firmware/i915/* /lib/firmware/i915/
 
-echo "ALL DONE, PLEASE REBOOT"
+echo "--- Updating initramfs ---"
+update-initramfs -u -k all
+
+echo "All done, please reboot."
